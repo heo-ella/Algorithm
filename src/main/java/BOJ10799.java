8f84e8d782606d93413a81ffd8b861633eb50bd5
heo-ella <yeseul2972@gmail.com>
2021-06-08 19:33:20 +0900

    BJ10799 쇠막대기

---
+++ src/main/java/BOJ10799.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BOJ10799 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String stick = scan.next();
+        int ans = 0;
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i=0; i<stick.length(); i++){
+            if(stick.charAt(i)=='(') {
+                stack.push('(');
+            }else{
+                // stick.chartAt(i)==')'
+                stack.pop();
+                if(stick.charAt(i-1)=='('){ // 레이저
+                    ans += stack.size();
+                }else{
+                    ans +=1;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
