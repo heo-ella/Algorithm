16d06816d421163ff3aa54a93e95705e8a7a9101
heo-ella <yeseul2972@gmail.com>
2021-06-27 18:09:52 +0900

    입축력문제: A+B -3

---
+++ src/main/java/BOJ10950.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BOJ10950 {
+    public static void main(String[] args)  {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            int A = in.nextInt();
+            int B = in.nextInt();
+            arr[i] = A+B;
+        }
+
+        in.close();
+
+        for(int out : arr){
+            System.out.println(out);
+        }
+    }
+}
