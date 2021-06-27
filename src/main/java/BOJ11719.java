fd3cfa13c29c3df74aa1bd16cc1ccb3318cc827e
heo-ella <yeseul2972@gmail.com>
2021-06-27 18:15:34 +0900

    입출력 문제: 그대로 출력하기2

---
+++ src/main/java/BOJ11719.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ11719 {
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+
+        while(in.hasNextLine()){
+            String word = in.nextLine();
+            if(word.length()>100) break;
+
+            words.add(word);
+        }
+        in.close();
+
+        for(int i=0; i< words.size() ; ++i) {
+            System.out.println(words.get(i));
+        }
+    }
+}
