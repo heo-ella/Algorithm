8f4ddd5c13a361580205804ee433c66213c5a9a5
heo-ella <yeseul2972@gmail.com>
2021-06-28 02:22:45 +0900

    알고리즘 기초: 덱(수정:scanner->BufferReader)

--- src/main/java/BOJ10866.java
+++ src/main/java/BOJ10866.java
@@ -1,23 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class BOJ10866 {
-    public static void main(String[] args){
-        LinkedList<Integer> deque = new LinkedList<>();
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> deque = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
 
         for(int i=0; i<N ; i++){
-            String input = in.next();
-            int value;
-            switch (input){
+            String[] input = br.readLine().split(" ");
+            switch (input[0]){
                 case "push_back":
-                    value = in.nextInt();
-                    deque.addLast(value);
+                    deque.addLast(Integer.parseInt(input[1]));
                     break;
                 case "push_front":
-                    value = in.nextInt();
-                    deque.addFirst(value);
+                    deque.addFirst(Integer.parseInt(input[1]));
                     break;
                 case "pop_front":
                     System.out.println(deque.isEmpty()?-1:deque.pop());
