7f909422c1d7d833c81c95494f934291f504a439
heo-ella <yeseul2972@gmail.com>
2021-06-27 18:12:05 +0900

    입출력 문제: A+B - 5

---
+++ src/main/java/BOJ10953.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ10953 {
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        for(int i=0; i<t ; i++){
+            String[] arr = in.next().split(",");
+            System.out.println(Integer.parseInt(arr[0])+Integer.parseInt(arr[1]));
+        }
+    }
+}
