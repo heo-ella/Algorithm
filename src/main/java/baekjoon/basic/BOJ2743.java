2ba7fad588050e0733243146fe2cafcaf6a7587b
heo-ella <yeseul2972@gmail.com>
2021-07-09 05:29:40 +0900

    알고리즘 기초 : 단어 길이 재기(간단코드)

--- src/main/java/baekjoon/basic/BOJ2743.java
+++ src/main/java/baekjoon/basic/BOJ2743.java
@@ -7,16 +7,6 @@ import java.io.InputStreamReader;
 public class BOJ2743 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        String input = br.readLine();
-        int output=0;
-
-        for(int i=0; i<input.length(); i++){
-            output++;
-        }
-
-        sb.append(output);
-        System.out.println(sb);
+        System.out.print(br.readLine().length());
     }
 }
